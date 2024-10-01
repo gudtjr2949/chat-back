708281cac76fa9cfe60263214955f36ce0539bd5
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2024-10-01 23:20:41 +0900

    Feat: AWS Parameter Store 기능 추가

--- chat-normal/src/test/java/com/example/chatnormal/ChatNormalApplicationTests.java
+++ chat-normal/src/test/java/com/example/chatnormal/ChatNormalApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.chatnormal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ChatNormalApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class ChatNormalApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

d4bbdb3cf09874af25412cc6b7725f8ba4bfb789
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2024-10-05 22:55:32 +0900

    Feat: Spring Cloud Gateway 적용

--- chat-normal/src/main/java/com/example/chatnormal/controller/GreetingController.java
+++ chat-normal/src/main/java/com/example/chatnormal/controller/GreetingController.java
@@ -27,8 +27,6 @@ public class GreetingController {
     public ResponseEntity<Void> greeting(@RequestBody ChatMessage chatMessage) throws Exception {
         // 전처리를 거쳐도 됨. DB, Redis 저장 등등
         Long chatroomId = chatMessage.getId();
-        chatroomId %= 5;
-        if (chatroomId == 0) chatroomId = 5L;
         chatMessage.setId(chatroomId);
         chatService.saveChat(chatMessage);
         template.convertAndSend("/sub/chatroom/" + chatMessage.getId(), chatMessage); // sub
@@ -37,25 +35,7 @@ public class GreetingController {
 
     @GetMapping("/chat/{chatroomId}")
     public ResponseEntity<ChatHistory> loadChatHistory(@PathVariable Long chatroomId) {
-        chatroomId %= 5;
-        if (chatroomId == 0) chatroomId = 5L;
         ChatHistory chatHistory = chatService.loadChatHistory(chatroomId);
         return ResponseEntity.ok().body(chatHistory);
     }
-
-//    @GetMapping("/main/{memberId}")
-//    public ModelAndView showMain(@PathVariable Long memberId) {
-//        ModelAndView mv = new ModelAndView();
-//        mv.setViewName("/main");
-//        mv.addObject("chatRooms", chatService.getChatRoomList(memberId));
-//        return mv;
-//    }
-
-//    @GetMapping("/chatroom/{chatroomId}")
-//    public ModelAndView getChatRoomList(@PathVariable Long chatroomId) {
-//        ModelAndView mv = new ModelAndView();
-//        mv.setViewName("/chatroom");
-//        mv.addObject("chatroomId", chatroomId);
-//        return mv;
-//    }
 }
